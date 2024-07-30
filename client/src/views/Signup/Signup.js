27953b929eb65b8365b5b6f3523f0d2669fe15f4
Aashikajain1 <jainaashika.a.1@gmail.com>
2024-07-30 22:07:37 +0530

    Signup Page added

---
+++ client/src/views/Signup/Signup.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import toast, {Toaster} from 'react-hot-toast'
+import { Link } from 'react-router-dom'
+
+function Signup() {
+
+  const [user, setUser] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+    dob: ''
+  })
+
+  const signup = async () => {
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/signup`, {
+      fullName: user.fullName,
+      email: user.email,
+      password: user.password,
+      dob: user.dob
+    })
+
+    if(response.data.success){
+      toast.success(response.data.message)
+
+      setUser({
+        fullName: '',
+        email: '',
+        password: '',
+        dob: ''
+      })
+    }
+    else{
+      toast.error(response.data.message)
+    }
+  }
+
+
+  return (
+    <div>
+      <h1 className='text-center'>User Registration</h1>
+
+      <form className='d-flex flex-column position-absolute top-50 start-50 translate-middle bg-dark' >
+        <input
+          type="text"
+          placeholder="Fullname"
+          className='mx-5 my-2 p-2'
+          value={user.fullName}
+          onChange={(e) => setUser({ ...user, fullName: e.target.value})}
+          />
+
+        <input
+          type="email"
+          placeholder="Email"
+          className='mx-5 my-2 p-2'
+          value={user.email}
+          onChange={(e)=>setUser({...user, email: e.target.value})}
+          />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className='mx-5 my-2 p-2'
+          value={user.password}
+          onChange={(e)=>setUser({...user, password: e.target.value})}
+          />
+
+        <input
+          type="date"
+          placeholder="Date of Birth"
+          className='mx-5 my-2 p-2'
+          value={user.dob}
+          onChange={(e)=>setUser({...user, dob: e.target.value})}
+          />
+
+        <button
+          type='button'
+          className='btn btn-primary  mx-5 my-2'
+          onClick={signup}
+          >
+          Register
+        </button>
+        <Link to='/login' className='text-center'>Already have an account? Login</Link>
+
+      </form>
+
+
+      <Toaster />
+    </div>
+  )
+}
+
+export default Signup
